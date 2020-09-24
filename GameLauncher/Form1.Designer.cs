6da03cc0c700555b69d52a59830481aba883d0b3
= <= 20mjchristian@gmail.com>
2020-09-24 19:35:32 -0400

    Created Launcher from tutorial

---
+++ GameLauncher/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace GameLauncher
+{
+    partial class GameLauncher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.barProgress = new System.Windows.Forms.ProgressBar();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(292, 253);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // barProgress
+            // 
+            this.barProgress.Location = new System.Drawing.Point(62, 371);
+            this.barProgress.Name = "barProgress";
+            this.barProgress.Size = new System.Drawing.Size(100, 23);
+            this.barProgress.TabIndex = 1;
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Location = new System.Drawing.Point(575, 397);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(35, 13);
+            this.lblVersion.TabIndex = 2;
+            this.lblVersion.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(259, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // GameLauncher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.barProgress);
+            this.Controls.Add(this.btnPlay);
+            this.Name = "GameLauncher";
+            this.Text = "Game Launcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.ProgressBar barProgress;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ GameLauncher/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace GameLauncher
+{
+    public enum LauncherStatus
+    {
+        ready,
+        failed,
+        downloadingGame,
+        downloadingUpdate
+    }
+    struct Version
+    {
+        public static Version zero = new Version(0, 0, 0);
+
+        public short major, minor, patch;
+        public Version(short major, short minor, short patch)
+        {
+            this.major = major;
+            this.minor = minor;
+            this.patch = patch;
+        }
+        public Version(string vers)
+        {
+            string[] parts = vers.Split('.');
+            if (parts.Length != 3)
+            {
+                major = 0;
+                minor = 0;
+                patch = 0;
+                Console.WriteLine("Version wasn't formatted correctly");
+                return;
+            }
+
+            major = short.Parse(parts[0]);
+            minor = short.Parse(parts[1]);
+            patch = short.Parse(parts[2]);
+        }
+        public static bool operator ==(Version a, Version b)
+        {
+            if (a.major != b.major)
+                return false;
+            if (a.minor != b.minor)
+                return false;
+            if (a.patch != b.patch)
+                return false;
+            return true;
+        }
+        public static bool operator !=(Version a, Version b) => !(a == b);
+        public override string ToString() => $"V{major}.{minor}.{patch}";
+
+    }
+    public partial class GameLauncher : Form
+    {
+        private string rootPath;
+        private string versionFile;
+        private string gameZip;
+        private string gameExe;
+        private string gameName = "Game.exe";
+
+        private string versionDL;
+        private string gameDL;
+
+        private LauncherStatus _status;
+        public LauncherStatus Status
+        {
+            get => _status;
+            set
+            {
+                _status = value;
+                switch (_status)
+                {
+                    case LauncherStatus.ready:
+                        btnPlay.Text = "Play";
+                        break;
+                    default:
+                    case LauncherStatus.failed:
+                        btnPlay.Text = "Update Failed - Retry";
+                        break;
+                    case LauncherStatus.downloadingGame:
+                        btnPlay.Text = "Downloading Game";
+                        break;
+                    case LauncherStatus.downloadingUpdate:
+                        btnPlay.Text = "Downloading Update";
+                        break;
+                }
+            }
+        }
+
+        public GameLauncher()
+        {
+            InitializeComponent();
+
+            rootPath = Directory.GetCurrentDirectory();
+            versionFile = Path.Combine(rootPath, "Version.txt");
+            gameZip = Path.Combine(rootPath, "Build.zip");
+            gameExe = Path.Combine(rootPath, "Build", gameName);
+            
+            CheckForUpdates();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(gameExe) && Status == LauncherStatus.ready)
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo(gameExe);
+                startInfo.WorkingDirectory = Path.Combine(rootPath, "Build");
+                Process.Start(startInfo);
+
+                Close();
+            }
+            else if (Status == LauncherStatus.failed)
+            {
+                CheckForUpdates();
+            }
+        }
+        private void CheckForUpdates()
+        {
+            if (File.Exists(versionFile))
+            {
+                Version localVersion = new Version(File.ReadAllText(versionFile));
+                lblVersion.Text = localVersion.ToString();
+
+                try
+                {
+                    WebClient webClient = new WebClient();
+                    Version onlineVersion = new Version(webClient.DownloadString(versionDL));
+
+                    if (onlineVersion != localVersion)
+                        InstallGameFiles(true, onlineVersion);
+                    else
+                        Status = LauncherStatus.ready;
+                }
+                catch (Exception ex)
+                {
+                    Status = LauncherStatus.failed;
+                    MessageBox.Show($"Error checking for game updates: {ex}");
+                }
+            }
+            else
+                InstallGameFiles(false, Version.zero);
+        }
+        private void InstallGameFiles(bool _isUpdate, Version _onlineVersion)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                if (_isUpdate)
+                {
+                    Status = LauncherStatus.downloadingUpdate;
+                }
+                else
+                {
+                    Status = LauncherStatus.downloadingGame;
+                    _onlineVersion = new Version(webClient.DownloadString(versionDL));
+                }
+
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadGameCompletedCallback);
+                webClient.DownloadFileAsync(new Uri(gameDL), gameZip, _onlineVersion);
+            }
+            catch (Exception ex)
+            {
+                Status = LauncherStatus.failed;
+                MessageBox.Show($"Error installing game files: {ex}");
+            }
+        }
+        private void DownloadGameCompletedCallback(object sender, AsyncCompletedEventArgs e)
+        {
+            try
+            {
+                string onlineVersion = ((Version)e.UserState).ToString();
+                ZipFile.ExtractToDirectory(gameZip, rootPath);
+                File.Delete(gameZip);
+
+                File.WriteAllText(versionFile, onlineVersion);
+
+                lblVersion.Text = onlineVersion;
+                Status = LauncherStatus.ready;
+            }
+            catch (Exception ex)
+            {
+                Status = LauncherStatus.failed;
+                MessageBox.Show($"Error finishing download: {ex}");
+            }
+        }
+
+    }
+}
